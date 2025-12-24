f2fa730107ab9737e49ec09eb2df3aa1c5b52f75
Atharva Vaddepalli <v.atharvan@gmail.com>
2025-12-25 03:35:32 +0530

    added on git

---
+++ Variables/Output.java
@@ -0,0 +1,6 @@
+class Output {
+    public static void main (String args []) {
+        System.out.println("'System.out.println()' is for getting output ");
+        System.out.print("Atharva");
+    }
+}
---
+++ basic/FirstClass.java
@@ -0,0 +1,6 @@
+// in java development kit there is a jre which is called as java runtim eenivrement and again in that ther is a one folder which is jam 
+class FirstClass {
+    public static void main (String args []) {
+               System.out.println("Hello world ");       //to print a line 
+    }
+}
