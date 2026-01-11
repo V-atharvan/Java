adda50e7f5a2920b5906b21fb52c8a20caf88532
Atharva Vaddepalli <v.atharvan@gmail.com>
2026-01-11 20:16:42 +0530

    java 1.1

---
+++ Variables/Printfun.java
@@ -0,0 +1,8 @@
+class Printfun {
+    public static void main(String[] args) {
+        System.out.println("This is used for printing ");
+        System.out.print("This is used for printing line in side by side ");
+        System.out.println(" is used for break the line ");
+        System.out.println("thats it ");
+    }
+}
