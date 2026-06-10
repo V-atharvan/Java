cd9c36248c91414d2385a92c2b41b444dd43481d
Atharva Vaddepalli <v.atharvan@gmail.com>
2026-06-10 19:42:43 +0530

    Function

---
+++ Function/src/Functionbasic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Functionbasic {
+
+    static int sum(int a ,int b){
+       int  c = a+b;
+      return c;
+    }
+
+    static void main(String[] args) {
+        System.out.println(sum(2,3));
+    }
+}
+git
