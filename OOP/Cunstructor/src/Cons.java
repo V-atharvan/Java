47b9eeb8ac37c519b2ef85e7a34edab07152823e
Atharva Vaddepalli <v.atharvan@gmail.com>
2026-06-16 10:21:51 +0530

    This key ward

--- OOP/Cunstructor/src/Cons.java
+++ OOP/Cunstructor/src/Cons.java
@@ -1,11 +1,31 @@
-import java.util.*;
-
 public class Cons {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        Vehical v = new Vehical("BMW");
+        Vehical v2 = new Vehical("Bmw", 232,"abc");
 
+        v.display();
+        v2.display();
     }
 }
 
 class Vehical{
+    String name;
+    int model;
+    String modelName;
 
+    Vehical(String name){
+        System.out.println(name);
+    }
+
+    Vehical(String name,int model,String modelName){
+        this.name = name;
+        this.model = model;
+        this.modelName = modelName;
+    }
+
+    void display() {
+        System.out.println("Brand: " + name);
+        System.out.println("Model Year: " + model);
+        System.out.println("Model Name: " + modelName);
+    }
 }
