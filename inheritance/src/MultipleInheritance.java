f95f861b1de5bf632d9df41012072e124ff40486
Atharva Vaddepalli <v.atharvan@gmail.com>
2026-06-25 20:35:07 +0530

    Multiple inheritance

---
+++ inheritance/src/MultipleInheritance.java
@@ -0,0 +1,33 @@
+public class MultipleInheritance {
+    static void main(String[] args) {
+        Student s = new Student();
+        MedicalStd m = new MedicalStd();
+        Engi e = new Engi();
+
+        s.stmt();
+
+        m.stmt();
+        m.attend();
+
+        e.stmt();
+        e.present();
+    }
+}
+
+class Student{
+    public void stmt(){
+        System.out.println("He is a student");
+    }
+}
+
+class MedicalStd extends Student {
+    public void attend () {
+        System.out.println("He is a medical student ");
+    }
+}
+
+class Engi extends Student{
+    public void present (){
+        System.out.println("He is a Engineering student ");
+    }
+}
